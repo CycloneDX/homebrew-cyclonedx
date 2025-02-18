ff994a8fce36a431c8c02aec432183d1d6982bad
Jan Kowalleck <jan.kowalleck@gmail.com>
2025-02-18 19:10:09 +0100

    chore/ci-test-formula
    Signed-off-by: Jan Kowalleck <jan.kowalleck@gmail.com>

--- .github/scripts/update-formula/gomod/gen.sh
+++ .github/scripts/update-formula/gomod/gen.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eu
 # Purpose: generate a forumla for "CycloneDX-gomod" 
 
 UF_REPO='CycloneDX/cyclonedx-gomod'
@@ -8,7 +8,7 @@ UF_VERSION="$1"
 THIS_DIR="$(realpath "$(dirname "$0")" )"
 TMP_DIR="$(mktemp -d "$THIS_DIR/.tmp.fu.XXXXXXXX")"
 
-formula="$(< "$THIS_DIR/template.rb")"
+template="$(< "$THIS_DIR/template.rb")"
 
 gh release download \
   "v${UF_VERSION}" \
@@ -21,7 +21,7 @@ gh release download \
 for _target in 'darwin_amd64' 'darwin_arm64' 'linux_arm64' 'linux_amd64' 
 do
   _sha256sum="$(sha256sum -b "${TMP_DIR}/cyclonedx-gomod_${UF_VERSION}_${_target}.tar.gz" | cut -f1 -d' ')"
-  formula=${formula//%SHA256SUM_${_target}%/$_sha256sum}
+  formula=${template//%SHA256SUM_${_target}%/$_sha256sum}
 done
 
 rm -rf "$TMP_DIR" 1>&2
