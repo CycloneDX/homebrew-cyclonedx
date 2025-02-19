5939070584862f2c72fe16bf09d7ef522461cc60
[BOT] Formula Updater <formula-updater@bot.local>
2025-02-19 12:05:24 +0000

    Update cli: 0.27.2
    Signed-off-by: [BOT] Formula Updater <formula-updater@bot.local>

--- Formula/cyclonedx-cli.rb
+++ Formula/cyclonedx-cli.rb
@@ -1,12 +1,12 @@
 class CyclonedxCli < Formula
-  desc "CycloneDX CLI tool for SBOM analysis, merging, diffs and format conversions."
+  desc "CLI tool for CycloneDX analysis, merging, diffs and format conversions"
   homepage "https://cyclonedx.org"
   version "0.27.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-osx-x64", :using => :nounzip
+      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-osx-x64", using: :nounzip
       sha256 "331c2245ef7dadf09fa3d2710a2aaab071ff6bea2ba3e5df8f95a4f3f6e825e9"
 
       def install
@@ -25,7 +25,7 @@ class CyclonedxCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-linux-arm64", :using => :nounzip
+      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-linux-arm64", using: :nounzip
       sha256 "5b4181f6fd4d8fbe54e55c1b3983d9af66ce2910a263814b290cbd5e351e68a4"
 
       def install
@@ -33,7 +33,7 @@ class CyclonedxCli < Formula
       end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-linux-arm", :using => :nounzip
+      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-linux-arm", using: :nounzip
       sha256 "46347273361b30c240c5ef14ba0963bbf4591cfbb030ea60da8488bc9a0d362e"
 
       def install
@@ -41,7 +41,7 @@ class CyclonedxCli < Formula
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-linux-x64", :using => :nounzip
+      url "https://github.com/CycloneDX/cyclonedx-cli/releases/download/v0.27.2/cyclonedx-linux-x64", using: :nounzip
       sha256 "5e1595542a6367378a3944bbd3008caab3de65d572345361d3b9597b1dbbaaa0"
 
       def install
@@ -51,6 +51,6 @@ class CyclonedxCli < Formula
   end
 
   test do
-    system "#{bin}/cyclonedx" "--version"
+    system bin/"cyclonedx", "--version"
   end
 end
